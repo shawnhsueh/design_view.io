04844b43ba02802de749107ec943d953eb596ff2
shawnhsueh <cttm4a1@gmail.com>
2022-05-26 23:37:45 -0700

    complete doc

---
+++ scripts/generate_md.py
@@ -0,0 +1,16 @@
+import glob
+
+file_list = glob.glob('/storage5/shawn/coronavirus/github/design_view.io/mol/*/*')
+with open('/storage5/shawn/coronavirus/github/design_view.io/scripts/tmp.md','r+') as f:
+    txt_original = f.read()
+for i in range(len(file_list)):
+    toks = file_list[i].split('/')
+    design_name = toks[-2]
+    pdb_name = toks[-1]
+    txt = txt_original.replace('TMP_identifier', design_name)
+    txt = txt.replace('TMP_pdb', design_name+'/'+pdb_name)
+    txt = txt.replace('TMP_order', str(i+1))
+    with open('/storage5/shawn/coronavirus/github/design_view.io/docs/S2/'+design_name+'.md','w+') as f:
+        f.write(txt)
+
+
